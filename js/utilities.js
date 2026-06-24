3159f4c5eb406eac46ce0ede1e09defbb0f13f08
Nikita Mikhaylov <n.mikhaylov@vardot.com>
2026-06-24 11:44:43 +0300

    Finished the Hero-Slider

--- js/utilities.js
+++ js/utilities.js
@@ -16,3 +16,60 @@ document.querySelectorAll('.more-link').forEach((link) => {
     text.className = 'more-link-text';
   });
 });
+
+
+
+// animates the numbers to increase gradually when loaded, found on https://codepen.io/lucamurante/pen/gZVymW
+
+
+function animateValue(obj, start = 0, end = null, duration = 800) {
+    if (obj) {
+
+        // save starting text for later (and as a fallback text if JS not running and/or google)
+        var textStarting = obj.innerHTML;
+
+        // remove non-numeric from starting text if not specified
+        end = end || parseInt(textStarting.replace(/\D/g, ""));
+
+        var range = end - start;
+
+        // no timer shorter than 50ms (not really visible any way)
+        var minTimer = 50;
+
+        // calc step time to show all interediate values
+        var stepTime = Math.abs(Math.floor(duration / range));
+
+        // never go below minTimer
+        stepTime = Math.max(stepTime, minTimer);
+
+        // get current time and calculate desired end time
+        var startTime = new Date().getTime();
+        var endTime = startTime + duration;
+        var timer;
+
+        function run() {
+            var now = new Date().getTime();
+            var remaining = Math.max((endTime - now) / duration, 0);
+            var value = Math.round(end - (remaining * range));
+            // replace numeric digits only in the original string
+            obj.innerHTML = textStarting.replace(/([0-9]+)/g, value);
+            if (value == end) {
+                clearInterval(timer);
+            }
+        }
+
+        timer = setInterval(run, stepTime);
+        run();
+    }
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      animateValue(entry.target);
+      observer.unobserve(entry.target); // run once only
+    }
+  });
+}, { threshold: 0.5 });
+
+document.querySelectorAll('.animate-number').forEach(el => observer.observe(el));
